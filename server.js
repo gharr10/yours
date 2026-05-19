7794c8ee04743927bdeaa5547ffba15962bf65e9
George Harrington <georgeharrington@Georges-MacBook-Air-3.local>
2026-05-19 18:48:51 +0100

    Fix double encoding

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ async function dbGet(key) {
 async function dbSet(key, value) {
   await supabase
     .from('store')
-    .upsert({ key, value: JSON.stringify(value) });
+    ..upsert({ key, value });
 }
 
 async function dbDel(key) {
